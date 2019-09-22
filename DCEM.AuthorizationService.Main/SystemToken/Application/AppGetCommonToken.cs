b2aefa320e7705fe917cb23ee51af23ce1a32ed1
MSBILL <76301101@qq.com>
2019-09-22 14:18:12 +0800

    更改app项目目录

---
+++ DCEM.AuthorizationService.Main/SystemToken/Application/AppGetCommonToken.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MSLibrary.LanguageTranslate;
+using MSLibrary.DI;
+using MSLibrary;
+using MSLibrary.SystemToken;
+
+namespace DCEM.AuthorizationService.Main.SystemToken.Application
+{
+    [Injection(InterfaceType = typeof(IAppGetCommonToken), Scope = InjectionScope.Singleton)]
+    public class AppGetCommonToken : IAppGetCommonToken
+    {
+        private readonly ISystemLoginEndpointRepository _systemLoginEndpointRepository;
+
+        public AppGetCommonToken(ISystemLoginEndpointRepository systemLoginEndpointRepository)
+        {
+            _systemLoginEndpointRepository = systemLoginEndpointRepository;
+        }
+        public async Task<string> Do(string systemLoginEndpointName, string authorizationEndpointName, string userName, string password)
+        {
+            var systemLoginEndpoint= await _systemLoginEndpointRepository.QueryByName(systemLoginEndpointName);
+            if (systemLoginEndpoint==null)
+            {
+                var fragment = new TextFragment()
+                {
+                    Code = TextCodes.NotFoundSystemLoginEndpointByName,
+                    DefaultFormatting = "找不到名称为{0}的系统登录终结点",
+                    ReplaceParameters = new List<object>() { systemLoginEndpointName }
+                };
+
+                throw new UtilityException((int)Errors.NotFoundSystemLoginEndpointByName, fragment);
+            }
+
+            var result= await systemLoginEndpoint.GetCommonToken(authorizationEndpointName, userName, password);
+            return result;
+        }
+    }
+}
---
+++ DCEM.AuthorizationService/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MSLibrary.SystemToken;
+
+namespace DCEM.AuthorizationService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public AuthController( ) {
+
+        }
+
+        //public async Task<string> GetCommonToken(HttpRequest request)
+        //{
+        //    return await Task.FromResult("OK");
+        //}
+        public async Task<GetCommonTokenResult> GetCommonToken(string authorizationName, string returnUrl)
+        {
+            return await Task.FromResult(new GetCommonTokenResult());
+        }
+
+        public async Task<string> GetCommonToken(string authorizationName, string userName, string password)
+        {
+            return await Task.FromResult("");
+        }
+    }
+}
