66eb701be847c0c96d28c2fa10eebbd71430dc4e
魏威 <karthur2020@outlook.com>
2019-10-22 23:02:03 +0800

    code update

--- DCEM.Web/Controllers/CustomerController.cs
+++ DCEM.Web/Controllers/CustomerController.cs
@@ -6,19 +6,26 @@ using MSLibrary.Xrm.Token;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
-
+using DCEM.ServiceAssistantService.Main.Application;
+using DCEM.ServiceAssistantService.Main.DAL;
 namespace DCEM.Web.Controllers
 {
     [Route("Api/Customer")]
     [EnableCors("any")]
     [ApiController]
-    public class CustomerController : Controller
+    public class CustomerController : ControllerBase
     {
+        ICustomerService _customerService;
+        public CustomerController()
+        {
+            _customerService = new CustomerFactory().Create().Result;
+        }
+
         [HttpGet]
         [Route("GetMyCustomerList")]
-        public ActionResult<string> GetMyCustomerList()
+        public ActionResult<CrmEntityCollection> GetMyCustomerList()
         {
-            return "11";
+            return _customerService.QueryList().Result; 
         }
 
 
