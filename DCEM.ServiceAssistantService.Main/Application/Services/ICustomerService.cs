136ea88c40ec80c1684002229399c5f4271f4ce8
魏威 <karthur2020@outlook.com>
2019-10-22 09:48:34 +0800

    code update

---
+++ DCEM.ServiceAssistantService.Main/Application/Services/ICustomerService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCEM.ServiceAssistantService.Main.Application
+{
+    public interface ICustomerService
+    {
+        /// <summary>
+        /// 查询客户列表
+        /// </summary>
+        void QueryList();
+    }
+}
