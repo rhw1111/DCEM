1c4b7decaefe5133911f9c0d7148dfa10a1db930
魏威 <karthur2020@outlook.com>
2019-10-24 14:16:35 +0800

    code update

--- DCEM.ServiceAssistantService.Main/Application/Services/CustomerService.cs
+++ DCEM.ServiceAssistantService.Main/Application/Services/CustomerService.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 using MSLibrary.Xrm.Message.RetrieveMultipleFetch;
 using System.Xml.Linq;
 using MSLibrary;
-
+using System;
 namespace DCEM.ServiceAssistantService.Main.Application
 {
     public class CustomerService : ICustomerService
@@ -14,8 +14,33 @@ namespace DCEM.ServiceAssistantService.Main.Application
             _crmService = crmService;
         }
 
-        public async Task<QueryResult<CrmEntity>> QueryList()
+        public async Task<QueryResult<CrmEntity>> QueryList(int type, int pageindex, string search)
         {
+            var filter = string.Empty;
+            if (listType == 2)
+            {
+                filter += $"<condition attribute='mcs_warrantyendtime' operator='le' value='{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}' />";
+            }
+            else if (listType == 3)
+            {
+                filter += $"<condition attribute='mcs_insuranceexpirationdate' operator='le' value='{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}' />";
+            }
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                filter += $"<filter type='or'>";
+                filter += $"<condition attribute='mcs_vehplate' operator='like' value='%{search}%' />";
+                filter += $"<condition attribute='mcs_mobilephone' operator='like' value='%{search}%' />";
+                filter += $"<condition attribute='mcs_fullname' operator='like' value='%{search}%' />";
+                filter += $"</filter>";
+            }
+
+            if (string.IsNullOrEmpty(filter))
+            {
+                filter = "<filter type='and'>" + filter;
+                filter = filter + "</filter>";
+            }
+
             var fetchString = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
   <entity name='mcs_carserviceadvisor'>
     <attribute name='mcs_name' />
@@ -34,6 +59,8 @@ namespace DCEM.ServiceAssistantService.Main.Application
     </link-entity>
   </entity>
 </fetch>";
+
+
             var fetchXdoc = XDocument.Parse(fetchString);
             var fetchRequest = new CrmRetrieveMultipleFetchRequestMessage()
             {
