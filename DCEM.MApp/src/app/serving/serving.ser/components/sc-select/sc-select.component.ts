116373f7d35d87cff06e43e3a3aba484ca470bf7
DESKTOP-UJ4AMUP\hexun <190563430@qq.com>
2019-10-29 15:32:07 +0800

    注释错误代码

--- DCEM.MApp/src/app/serving/serving.ser/components/sc-select/sc-select.component.ts
+++ DCEM.MApp/src/app/serving/serving.ser/components/sc-select/sc-select.component.ts
@@ -22,7 +22,50 @@ export class ScSelectComponent implements OnInit {
   }
 
   search(){
+    // this._page.loadingShow();
+    //     this.mod.data = [];
+    //     this._http.get(
+    //         this.mod.apiUrl,
+    //         {
+    //             params: {
+    //                 type: this.mod.searchData.type,
+    //                 pageindex: this.mod.searchData.pageindex,
+    //                 search: this.mod.searchData.search
+    //             }
+    //         },
+    //         (res: any) => {
+    //             if (res.Results !== null) {
+             
+    //                 for (var key in res.Results) {
+    //                     var obj = {};
+    //                     obj["Id"] = res.Results[key]["Id"];
+    //                     obj["fullname"] = res.Results[key]["Attributes"]["a_x002e_mcs_fullname"];
+    //                     obj["gender"] = res.Results[key]["Attributes"]["a_x002e_mcs_gender@OData.Community.Display.V1.FormattedValue"];
+    //                     obj["genderval"] = res.Results[key]["Attributes"]["a_x002e_mcs_gender"];
+    //                     obj["mobilephone"] = res.Results[key]["Attributes"]["a_x002e_mcs_mobilephone"];
+    //                     obj["vehplate"] = res.Results[key]["Attributes"]["a_x002e_mcs_vehplate"];
+    //                     obj["vehtype"] = res.Results[key]["Attributes"]["a_x002e_mcs_vehtype@OData.Community.Display.V1.FormattedValue"];
 
+    //                     this.mod.data.push(obj);
+    //                 }
+
+    //                 this.mod.allTotalCount = res.ALLTotalCount;
+    //                 this.mod.warrantyTotalCount = res.WarrantyTotalCount;
+    //                 this.mod.insuranceTotalCount = res.InsuranceTotalCount;
+
+
+    //                 this._page.loadingHide();
+    //             }
+    //             else {
+    //                 this._page.alert("消息提示", "客户数据加载异常");
+    //                 this._page.loadingHide();
+    //             }
+    //         },
+    //         (err: any) => {
+    //             this._page.alert("消息提示", "客户数据加载异常");
+    //             this._page.loadingHide();
+    //         }
+    //     );
   }
   
   dismissModal() {
