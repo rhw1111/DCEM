dce10d8cbfcd9bfe9583b3fb14822b86fc93c97a
熊桃 <xt791462904@outlook.com>
2019-11-05 14:05:16 +0800

    公共组件添加

---
+++ DCEM.MApp/src/app/serving/serving.ser/components/select-reservationconfiguration/select-reservationconfiguration.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-reservationconfiguration',
+  templateUrl: './select-reservationconfiguration.component.html',
+  styleUrls: ['./select-reservationconfiguration.component.scss'],
+})
+export class SelectReservationconfigurationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
