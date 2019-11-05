548dbccec8f4a28244cde853a365eee91d36e4cb
DESKTOP-UJ4AMUP\hexun <190563430@qq.com>
2019-11-05 16:14:54 +0800

    添加首页导航

--- DCEM.MApp/dist/out-tsc/src/app/app.module.js
+++ DCEM.MApp/dist/out-tsc/src/app/app.module.js
@@ -9,15 +9,23 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { IonicStorageModule } from '@ionic/storage';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 //自定义组件
 import { ScSelectComponent } from './serving/serving.ser/components/sc-select/sc-select.component';
 import { SelectCustomerComponent } from 'app/serving/serving.ser/components/select-customer/select-customer.component';
 import { SelectRepairlocationComponent } from 'app/serving/serving.ser/components/select-repairlocation/select-repairlocation.component';
 import { SelectCarmodelComponent } from 'app/serving/serving.ser/components/select-carmodel/select-carmodel.component';
+import { SelectSystemuserComponent } from 'app/base/base.ser/components/select-systemuser/select-systemuser.component';
 import { SelectMaintenanceComponent } from 'app/serving/serving.ser/components/select-maintenance/select-maintenance.component';
+import { SelectAppointmentconfigComponent } from 'app/serving/serving.ser/components/select-appointmentconfig/select-appointmentconfig.component';
 import { SelectPartsComponent } from 'app/serving/serving.ser/components/select-parts/select-parts.component';
 import { SelectRepairitemComponent } from 'app/serving/serving.ser/components/select-repairitem/select-repairitem.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SelectMalFunctionTypeComponent } from 'app/serving/serving.ser/components/select-malfunctiontype/select.malfunctiontype.component';
+import { SelectReceptioncommissionerComponent } from 'app/serving/serving.ser/components/select-receptioncommissioner/select-receptioncommissioner.component';
+import { SelectReservationconfigurationComponent } from 'app/serving/serving.ser/components/select-reservationconfiguration/select-reservationconfiguration.component';
+import { SelectSysareaComponent } from 'app/serving/serving.ser/components/select-sysarea/select-sysarea.component';
+import { SelectVehiclecolorComponent } from 'app/serving/serving.ser/components/select-vehiclecolor/select-vehiclecolor.component';
+import { SelectVehicletypeComponent } from 'app/serving/serving.ser/components/select-vehicletype/select-vehicletype.component';
 let AppModule = class AppModule {
 };
 AppModule = tslib_1.__decorate([
@@ -29,8 +37,17 @@ AppModule = tslib_1.__decorate([
             SelectRepairlocationComponent,
             SelectCarmodelComponent,
             SelectMaintenanceComponent,
+            SelectSystemuserComponent,
+            SelectAppointmentconfigComponent,
+            SelectMalFunctionTypeComponent,
             SelectPartsComponent,
-            SelectRepairitemComponent
+            SelectRepairitemComponent,
+            SelectMalFunctionTypeComponent,
+            SelectVehicletypeComponent,
+            SelectReceptioncommissionerComponent,
+            SelectReservationconfigurationComponent,
+            SelectSysareaComponent,
+            SelectVehiclecolorComponent
         ],
         entryComponents: [
             ScSelectComponent,
@@ -38,8 +55,17 @@ AppModule = tslib_1.__decorate([
             SelectRepairlocationComponent,
             SelectCarmodelComponent,
             SelectMaintenanceComponent,
+            SelectSystemuserComponent,
+            SelectAppointmentconfigComponent,
+            SelectMalFunctionTypeComponent,
             SelectPartsComponent,
-            SelectRepairitemComponent
+            SelectRepairitemComponent,
+            SelectMalFunctionTypeComponent,
+            SelectVehicletypeComponent,
+            SelectReceptioncommissionerComponent,
+            SelectReservationconfigurationComponent,
+            SelectSysareaComponent,
+            SelectVehiclecolorComponent
         ],
         imports: [BrowserModule,
             IonicModule.forRoot({ mode: 'ios' }),
