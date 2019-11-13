1db754809f6094c19d5bb72857b9bb18da4d0efa
熊桃 <xt791462904@outlook.com>
2019-11-13 16:50:04 +0800

    开票信息，上牌信息后端接口添加

---
+++ DCEM.Web/Controllers/VehnetworkController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using MSLibrary.Xrm;
+using MSLibrary;
+using MSLibrary.DI;
+using System;
+using Microsoft.AspNetCore.Http;
+using DCEM.SalesAssistant.Main.ViewModel.Request;
+using DCEM.SalesAssistant.Main.ViewModel.Response;
+using System.Threading.Tasks;
+using DCEM.SalesAssistant.Main.Application.App;
+using DCEM.SalesAssistant.Main.Factory;
+using DCEM.SalesAssistant.Main.Application.App.Contrac;
+
+namespace DCEM.Web.Controllers
+{
+    [Route("api/vehnetwork")]
+    [EnableCors("any")]
+    [ApiController]
+    public class VehnetworkController : ApiController
+    {
+
+        private IAppVehnetwork _appVehnetwork;
+        public VehnetworkController()
+        {
+            _appVehnetwork = new  VehnetworkFactory().Create().Result;
+        }
+        [Route("getlist")]
+        [HttpPost]
+        public async Task<NewtonsoftJsonActionResult<QueryResult<CrmEntity>>> getlist(VehnetworkListRequest request)
+        {
+            return await _appVehnetwork.getlist(request);
+        }
+
+        [Route("getdetail")]
+        [HttpPost]
+        public async Task<NewtonsoftJsonActionResult<VehnetworkDetailRepository>> get(Guid id)
+        {
+            return await _appVehnetwork.getdetail(id);
+        }
+
+        
+    }
+}
+
+
