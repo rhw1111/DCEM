6e3ed853e48376fdeb801bc5114b242a4ce6946a
刘俊 <liujunABCDE@outlook.com>
2019-11-20 11:40:24 +0800

    培育任务完成操作

--- DCEM.MApp/src/app/saleing/mcs-cultivatetask.com/detail/detail.page.ts
+++ DCEM.MApp/src/app/saleing/mcs-cultivatetask.com/detail/detail.page.ts
@@ -11,7 +11,8 @@ import { OptionSetService } from '../../saleing.ser/optionset.service';
 export class DetailPage implements OnInit {
 
   model = {
-    apiUrlInfo: '/api/only-lead/GetAcvitityDetail',
+    apiUrlInfo: '/api/only-lead/GetAcvitityDetail',//培育任务详情接口
+    addoreditUrl: '/api/activity/addoredit',//培育任务完成按钮接口
     activityData: {
       mcs_activityid: "", //主键id
       mcs_name: "", //主题
@@ -103,6 +104,44 @@ export class DetailPage implements OnInit {
   }
 
 
+  //任务完成
+  TaskFinish(){
+ 
+    this._page.confirm("确认提示", "确定完成该任务？",function(){
+        
+      
+    
+    });
 
+    var postData = {};
+    postData["mcs_activitystatus"] = 1;
+    
+    postData["mcs_endtime"] = new Date();
+
+    this._page.loadingShow();
+    this._http.post(
+      this.model.addoreditUrl,
+      postData,
+      (res: any) => {
+        this._page.loadingHide();
+        console.log(res);
+        if (res.Result == true) {
+          const that = this;
+          this._page.alert("消息提示", "操作成功", function () {
+            that._page.goBack();
+          });
+        }
+        else {
+          this._page.alert("消息提示", "操作失败");
+        }
+      },
+      (err: any) => {
+        this._page.loadingHide();
+        this._page.alert("消息提示", "操作失败");
+      }
+    );
+
+
+  }
 
 }
