d6b62a7fcf23fc5759b021c4acd752233cf90399
熊桃 <xt791462904@outlook.com>
2019-11-26 10:33:34 +0800

    框架加载

---
+++ DCEM.APP.UserCenter/www/community-index-index-module-es5.js
@@ -0,0 +1,103 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["community-index-index-module"],{
+
+/***/ "./node_modules/raw-loader/index.js!./src/app/page/community/index/index.page.html":
+/*!********************************************************************************!*\
+  !*** ./node_modules/raw-loader!./src/app/page/community/index/index.page.html ***!
+  \********************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<ion-header style=\"height:70px;background-color:rgb(254,251,255)\" no-border>\r\n    <ion-toolbar>\r\n        <ion-buttons slot=\"start\">\r\n            <svg t=\"1574667632246\" class=\"icon\" viewBox=\"0 0 1024 1024\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" p-id=\"2509\" width=\"32\" height=\"32\"><path d=\"M612.807111 796.501333c-5.12 63.829333-61.553778 113.777778-129.194667 113.777778s-124.074667-49.948444-129.194666-113.777778H142.279111a28.444444 28.444444 0 0 1-22.869333-45.340444l82.716444-112.014222c10.865778-14.734222 16.725333-32.483556 16.725334-50.801778V416.881778c0-167.025778 154.055111-293.944889 321.024-258.787556 122.083556 25.656889 208.497778 139.889778 208.497777 268.629334v161.621333c0 18.261333 5.859556 36.067556 16.725334 50.744889l82.716444 112.071111a28.444444 28.444444 0 0 1-22.869333 45.340444h-212.138667z m-57.230222 0h-143.928889c5.063111 31.687111 35.214222 56.888889 71.964444 56.888889s66.844444-25.201778 71.964445-56.888889z m57.628444-56.888889h155.363556l-49.208889-66.673777a141.880889 141.880889 0 0 1-27.875556-84.593778V426.723556c0-102.627556-68.380444-193.024-163.328-212.992-131.413333-27.648-252.416 72.021333-252.416 203.150222v171.463111c0 30.435556-9.784889 60.074667-27.875555 84.593778l-49.208889 66.673777h414.549333z\" fill=\"#2c2c2c\" p-id=\"2510\"></path><path d=\"M483.612444 82.432a28.444444 28.444444 0 0 1 28.444445 28.444444v63.260445a28.444444 28.444444 0 0 1-56.888889 0V110.876444a28.444444 28.444444 0 0 1 28.444444-28.444444z\" fill=\"#2c2c2c\" p-id=\"2511\"></path></svg>\r\n        </ion-buttons>\r\n        <ion-title style=\"background-color:rgb(254,251,255)\">\r\n            <ion-grid style=\"padding-left:60px;padding-right:60px;\">\r\n                <ion-row>\r\n                    <ion-col>\r\n                        <div  style=\" border-bottom:5px solid rgb(78,173,246)\">推荐</div>\r\n                    </ion-col>\r\n                    <ion-col>\r\n                        朋友圈\r\n                    </ion-col>\r\n                    <ion-col>\r\n                        问答\r\n                    </ion-col>\r\n                    <ion-col>\r\n                        活动\r\n                    </ion-col>\r\n                </ion-row>\r\n            </ion-grid>\r\n        </ion-title>\r\n        <ion-buttons slot=\"end\">\r\n            <svg t=\"1574667688408\" class=\"icon\" viewBox=\"0 0 1024 1024\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" p-id=\"3551\" width=\"32\" height=\"32\"><path d=\"M793.088 908.288c0 17.92-14.336 32.256-32.256 32.256h-640c-17.92 0-32.256-14.336-32.256-32.256V331.776h224.256V108.032h448.512c17.92 0 32.256 14.336 32.256 32.256v32.256h64v-32.256c0-52.736-43.008-96.256-96.256-96.256H235.52L24.576 254.464v653.824c0 52.736 43.008 96.256 96.256 96.256h640.512c52.736 0 96.256-43.008 96.256-96.256v-320H793.6v320zM248.832 120.832v146.944H101.888l146.944-146.944z\" fill=\"#1296db\" p-id=\"3552\"></path><path d=\"M970.752 261.632c-159.232-46.08-290.304-30.208-389.632 47.104-187.904 146.432-188.416 458.24-188.416 471.552h63.488c0-1.024 0-53.248 12.288-122.368 21.504 5.632 69.12 13.312 112.128-9.728 23.552-12.8 41.472-31.744 53.248-57.344 40.96 6.656 76.288 0 105.472-19.968 34.304-23.552 50.688-59.904 57.856-84.48 62.976-9.216 159.232-54.784 195.072-184.32l8.704-31.232-30.208-9.216zM773.12 422.912l-28.672-0.512-3.584 29.184c0 0.512-5.632 44.032-36.352 65.024-19.456 13.312-47.104 15.36-81.92 5.12l-30.208-8.704-8.704 31.232c-6.144 22.528-16.896 37.888-32.256 46.592-25.088 13.824-57.344 7.168-69.12 4.096 22.016-83.456 62.464-175.616 137.216-233.984 75.776-59.392 177.152-74.24 301.056-45.056-45.056 105.472-143.36 107.008-147.456 107.008z\" fill=\"#1296db\" p-id=\"3553\"></path></svg>\r\n        </ion-buttons>\r\n    </ion-toolbar>\r\n</ion-header>\r\n\r\n<ion-content style=\"overflow:no-display\">\r\n    <ion-slides style=\"height:100%\">\r\n        <ion-slide style=\"height:100%\">\r\n            <div style=\"width:100%; height:100%;overflow:scroll;overflow-y:auto;overflow-x:hidden\">\r\n                <ion-grid>\r\n                    <ion-row>\r\n                        <ion-col>\r\n                            头\r\n                            <br />\r\n                            <img src=\"../assets/img/1png.png\" />\r\n                            <img src=\"../assets/img/1png.png\" />\r\n                            <img src=\"../assets/img/1png.png\" />\r\n                            <img src=\"../assets/img/1png.png\" />\r\n                            <br />\r\n                            尾\r\n                        </ion-col>\r\n                    </ion-row>\r\n                </ion-grid>\r\n            </div>\r\n        </ion-slide>\r\n        <ion-slide style=\"height:100%\">\r\n            <div style=\"width:100%; height:100%;overflow:scroll;overflow-y:auto;overflow-x:hidden\">\r\n                <ion-grid>\r\n                    <ion-row>\r\n                        <ion-col>\r\n                            头\r\n\r\n                            尾\r\n                        </ion-col>\r\n                    </ion-row>\r\n                </ion-grid>\r\n            </div>\r\n        </ion-slide>\r\n    </ion-slides>\r\n</ion-content>\r\n"
+
+/***/ }),
+
+/***/ "./src/app/page/community/index/index.module.ts":
+/*!******************************************************!*\
+  !*** ./src/app/page/community/index/index.module.ts ***!
+  \******************************************************/
+/*! exports provided: IndexPageModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "IndexPageModule", function() { return IndexPageModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @ionic/angular */ "./node_modules/@ionic/angular/dist/fesm5.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/common */ "./node_modules/@angular/common/fesm5/common.js");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm5/forms.js");
+/* harmony import */ var _index_page__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./index.page */ "./src/app/page/community/index/index.page.ts");
+
+
+
+
+
+
+
+var IndexPageModule = /** @class */ (function () {
+    function IndexPageModule() {
+    }
+    IndexPageModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_3__["NgModule"])({
+            imports: [
+                _ionic_angular__WEBPACK_IMPORTED_MODULE_1__["IonicModule"],
+                _angular_common__WEBPACK_IMPORTED_MODULE_4__["CommonModule"],
+                _angular_forms__WEBPACK_IMPORTED_MODULE_5__["FormsModule"],
+                _angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"].forChild([{ path: '', component: _index_page__WEBPACK_IMPORTED_MODULE_6__["IndexPage"] }])
+            ],
+            declarations: [_index_page__WEBPACK_IMPORTED_MODULE_6__["IndexPage"]]
+        })
+    ], IndexPageModule);
+    return IndexPageModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/page/community/index/index.page.scss":
+/*!******************************************************!*\
+  !*** ./src/app/page/community/index/index.page.scss ***!
+  \******************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "* {\n  margin: 0px;\n  padding: 0px;\n}\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNyYy9hcHAvcGFnZS9jb21tdW5pdHkvaW5kZXgvQzpcXOW3peS9nFxc6aG555uuXFxhcHBcXERDRU0uQVBQLlVzZXJDZW50ZXIvc3JjXFxhcHBcXHBhZ2VcXGNvbW11bml0eVxcaW5kZXhcXGluZGV4LnBhZ2Uuc2NzcyIsInNyYy9hcHAvcGFnZS9jb21tdW5pdHkvaW5kZXgvaW5kZXgucGFnZS5zY3NzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0VBQ0ksV0FBQTtFQUNBLFlBQUE7QUNDSiIsImZpbGUiOiJzcmMvYXBwL3BhZ2UvY29tbXVuaXR5L2luZGV4L2luZGV4LnBhZ2Uuc2NzcyIsInNvdXJjZXNDb250ZW50IjpbIioge1xyXG4gICAgbWFyZ2luOiAwcHg7XHJcbiAgICBwYWRkaW5nOiAwcHg7XHJcbn1cclxuXHJcbiIsIioge1xuICBtYXJnaW46IDBweDtcbiAgcGFkZGluZzogMHB4O1xufSJdfQ== */"
+
+/***/ }),
+
+/***/ "./src/app/page/community/index/index.page.ts":
+/*!****************************************************!*\
+  !*** ./src/app/page/community/index/index.page.ts ***!
+  \****************************************************/
+/*! exports provided: IndexPage */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "IndexPage", function() { return IndexPage; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+
+
+var IndexPage = /** @class */ (function () {
+    function IndexPage() {
+    }
+    IndexPage = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+            selector: 'app-index',
+            template: __webpack_require__(/*! raw-loader!./index.page.html */ "./node_modules/raw-loader/index.js!./src/app/page/community/index/index.page.html"),
+            styles: [__webpack_require__(/*! ./index.page.scss */ "./src/app/page/community/index/index.page.scss")]
+        }),
+        tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [])
+    ], IndexPage);
+    return IndexPage;
+}());
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=community-index-index-module-es5.js.map
