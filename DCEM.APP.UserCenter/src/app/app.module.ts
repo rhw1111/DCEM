ea860d1c90ac8f5a552d7bb9fc7ed93ebf0e7a4d
魏威 <karthur2020@outlook.com>
2019-11-26 15:53:07 +0800

    code update

--- DCEM.APP.UserCenter/src/app/app.module.ts
+++ DCEM.APP.UserCenter/src/app/app.module.ts
@@ -6,27 +6,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
-import { IonicStorageModule } from '@ionic/storage';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
     entryComponents: [],
     imports: [
+        BrowserModule,
         IonicModule.forRoot({ mode: 'ios' }),
-        AppRoutingModule,
-        HttpClientModule,
-        IonicStorageModule.forRoot(),
-        FormsModule,
-        ReactiveFormsModule,
-    ],
+        AppRoutingModule],
     providers: [
         StatusBar,
         SplashScreen,
-        ScreenOrientation,
         { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
     ],
     bootstrap: [AppComponent]
