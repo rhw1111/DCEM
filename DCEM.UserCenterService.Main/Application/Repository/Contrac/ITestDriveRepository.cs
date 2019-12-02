8e20a8dac142b16d546b9b9f2c6f6f98fd364e9e
刘俊 <liujunABCDE@outlook.com>
2019-12-02 17:27:37 +0800

    页面调整

--- DCEM.UserCenterService.Main/Application/Repository/Contrac/ITestDriveRepository.cs
+++ DCEM.UserCenterService.Main/Application/Repository/Contrac/ITestDriveRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DCEM.UserCenterService.Main.ViewModel.Request;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,11 @@ namespace DCEM.UserCenterService.Main.Application.Repository.Contrac
 {
     public interface ITestDriveRepository
     {
+        /// <summary>
+        /// 我的试乘试驾预约xml
+        /// </summary>
+        /// <param name="Request"></param>
+        /// <returns></returns>
+        string GetDriveRecordList(TestDriveRequest Request);
     }
 }
