1cbc5cde2fe443d52cff85b1e3e8dc327daf87d0
魏威 <karthur2020@outlook.com>
2019-12-02 14:41:49 +0800

    code update

---
+++ DCEM.APP.UserCenter/src/app/page/carcenter/carstore/component/speclist/speclist.component.ts
@@ -0,0 +1,21 @@
+﻿import { Component, OnInit, ViewChild } from '@angular/core';
+import { ModalController, IonContent, NavParams, IonInfiniteScroll } from '@ionic/angular';
+
+@Component({
+    selector: 'app-speclist',
+    templateUrl: './speclist.component.html',
+    styleUrls: ['./speclist.component.scss'],
+})
+export class SpeclistComponent implements OnInit {
+
+    constructor(
+        private _modalCtrl: ModalController
+    ) { }
+
+    ngOnInit() { }
+
+    closeModal() {
+        this._modalCtrl.dismiss({
+        });
+    }
+}
