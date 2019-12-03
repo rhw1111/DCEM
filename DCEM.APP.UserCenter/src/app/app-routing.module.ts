a72dd77250b30297ac7937c49d8c73ca0e08c90e
魏威 <karthur2020@outlook.com>
2019-12-03 10:31:22 +0800

    code update

--- DCEM.APP.UserCenter/src/app/app-routing.module.ts
+++ DCEM.APP.UserCenter/src/app/app-routing.module.ts
@@ -63,8 +63,8 @@ const routes: Routes = [
         children: [
             { path: 'index', loadChildren: () => import('./page/message/index/index.module').then(m => m.IndexPageModule) },
             { path: 'room', loadChildren: () => import('./page/message/room/room.module').then(m => m.RoomPageModule) },
-            { path: 'sys', loadChildren: () => import('./page/message/sys/sys.module').then(m => m.SysPageModule) },
-            { path: 'detail', loadChildren: () => import('./page/message/detail/detail.module').then(m => m.DetailPageModule) },
+            { path: 'sys', loadChildren: () => import('./page/message/sys/sys.module').then(m => m.SysPageModule) }
+
         ]
     },
     {
