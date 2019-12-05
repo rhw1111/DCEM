916dbe50d534259050d1ea216ac3e8d809986aa4
魏威 <karthur2020@outlook.com>
2019-12-05 11:55:31 +0800

    code update

---
+++ DCEM.UserCenterService.Main/Application/App/Contrac/IStoreService.cs
@@ -0,0 +1,14 @@
+namespace DCEM.UserCenterService.Main.Application.App.Contrac
+{
+    using DCEM.UserCenterService.Main.Application.App.Contrac;
+    using DCEM.UserCenterService.Main.Application.Services.Contrac;
+    using DCEM.UserCenterService.Main.ViewModel.Request;
+    using DCEM.UserCenterService.Main.ViewModel.Response;
+    using System.Threading.Tasks;
+    using MSLibrary.Xrm;
+
+
+    public interface IStoreService
+    {
+    }
+}
---
+++ DCEM.UserCenterService.Main/Factory/StoreFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using MSLibrary;
+using DCEM.UserCenterService.Main.Application.App.Contrac;
+using DCEM.UserCenterService.Main.Application.App;
+using MSLibrary.Xrm;
+using DCEM.UserCenterService.Main;
+namespace DCEM.ServiceAssistantService.Main.DAL
+{
+    #region 商城接口
+    public class StoreFactory : IFactory<Task<IStoreService>>
+    {
+        ICrmService _crmService;
+        IStoreService _storeService;
+        public async Task<IStoreService> Create()
+        {
+            _crmService = StartupHelper.CreateCrmService();
+            return await Task<IStoreService>.Run(() =>
+            {
+                _storeService = new StoreService(_crmService);
+                return _storeService;
+            });
+        }
+    }
+    #endregion
+}
