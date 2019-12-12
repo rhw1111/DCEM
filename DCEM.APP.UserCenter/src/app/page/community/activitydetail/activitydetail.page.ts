7edd06e44fdca26dad88a3040286ae6b7d4b6851
王致远 <wangzhiyuanboluo@hotmail.com>
2019-12-12 11:21:22 +0800

    活动详情部分调整

--- DCEM.APP.UserCenter/src/app/page/community/activitydetail/activitydetail.page.ts
+++ DCEM.APP.UserCenter/src/app/page/community/activitydetail/activitydetail.page.ts
@@ -12,6 +12,7 @@ export class ActivitydetailPage implements OnInit {
 
   model = {
     contentManagementApiUrl: 'api/ContentManagement/GetContentDetail',
+
     params: {
       Type: 1,
       Id: ""
@@ -20,6 +21,18 @@ export class ActivitydetailPage implements OnInit {
       Title: "",
       PageUrl: null
     }
+    // ,
+    // signUpUrl: 'api/Lead/Do',
+    // signUpParams: {
+    //   UserId: "",
+    //   MobilePhone: "",
+    //   CampaignCode: "123",
+    //   //以下暂时固定
+    //   Channel: 3,
+    //   MediaCode: "app",
+    //   TerminalCode: "mobilephone",
+    //   BehaviorCode: "activity_online_registration"
+    // }
   }
 
   handler: any;
@@ -39,13 +52,14 @@ export class ActivitydetailPage implements OnInit {
     });
 
     //初始化事件
-    var _this = this;
-    this.handler = function (event) {
-      if (event != null) {
-        _this.setIframeValue(event.data);
-      }
-    };
-    this.addIFrameEvent();
+    // var _this = this;
+    // this.handler = function (event) {
+    //   if (event != null) {
+    //     _this.setIframeValue(event.data);
+    //   }
+    // };
+
+    // this.addIFrameEvent();
   }
 
   ionViewWillEnter() {
@@ -81,17 +95,67 @@ export class ActivitydetailPage implements OnInit {
     );
   }
 
-  setIframeValue(data) {
-    console.log(data);
-  }
+  // setIframeValue(data) {
+  //   if (typeof data.type != 'undefined') {
+  //     return false;
+  //   }
+  //   //活动报名逻辑开始
+  //   //校验
+  //   if (this.isEmpty(data.name)) {
+  //     this._page.alert("消息提示", "请输入姓名");
+  //     return false;
+  //   }
+  //   if (this.isEmpty(data.moblie)) {
+  //     this._page.alert("消息提示", "请输入手机号");
+  //     return false;
+  //   }
+  //   this._page.loadingShow();
 
-  addIFrameEvent() {
-    //注册新的事件
-    window.addEventListener('message', this.handler);
-  }
+  //   //组装参数
+  //   this.model.signUpParams.MobilePhone = data.mobile;
 
-  removeIFrameEvent() {
-    //移除之前的事件
-    window.removeEventListener('message', this.handler);
-  }
+  //   this._http.postForCC(
+  //     this.model.signUpUrl,
+  //     this.model.signUpParams,
+  //     (res: any) => {
+  //       //debugger;
+  //       if (res !== null) {
+  //         console.log(res);
+  //       }
+  //       else {
+  //         this._page.alert("消息提示", "数据处理异常");
+  //       }
+  //       this._page.loadingHide();
+  //     },
+  //     (err: any) => {
+  //       this._page.alert("消息提示", "数据处理异常");
+  //       this._page.loadingHide();
+  //     }
+  //   );
+  // }
+
+  // addIFrameEvent() {
+  //   //注册新的事件
+  //   window.addEventListener('message', this.handler);
+  // }
+
+  // removeIFrameEvent() {
+  //   //移除之前的事件
+  //   window.removeEventListener('message', this.handler);
+  // }
+
+  // isEmpty(obj) {
+  //   if (obj === null) return true;
+  //   if (typeof obj === 'undefined') {
+  //     return true;
+  //   }
+  //   if (typeof obj === 'string') {
+  //     if (obj === "") {
+  //       return true;
+  //     }
+  //     var reg = new RegExp("^([ ]+)|([　]+)$");
+  //     return reg.test(obj);
+  //   }
+  //   return false;
+  // }
 }
