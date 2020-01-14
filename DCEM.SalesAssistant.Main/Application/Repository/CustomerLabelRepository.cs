20316b6cfcd350b97bf09206838b8f0c57b90123
zuozhong <zz517115214@outlook.com>
2020-01-14 18:03:56 +0800

    获取客户标签过滤动态标签

--- DCEM.SalesAssistant.Main/Application/Repository/CustomerLabelRepository.cs
+++ DCEM.SalesAssistant.Main/Application/Repository/CustomerLabelRepository.cs
@@ -9,8 +9,13 @@ namespace DCEM.SalesAssistant.Main.Application.Repository
 {
     public class CustomerLabelRepository: ICustomerLabelRepository
     {
+        /// <summary>
+        /// 获取客户标签(过滤动态标签)
+        /// </summary>
+        /// <returns></returns>
         public async Task<XDocument> GetAll()
         {
+            //<condition attribute='mcs_taggroupid' operator='ne' value='E6E6CC76-F1BE-4C66-9695-D43CBA9589ED'/>
             return await Task<XDocument>.Run(() =>
             {
                 var fetchXml = $@"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
@@ -22,6 +27,11 @@ namespace DCEM.SalesAssistant.Main.Application.Repository
     <filter type='and'>
       <condition attribute='statecode' operator='eq' value='0' />
     </filter>
+    <link-entity name='mcs_taggroup' alias='ab' link-type='inner' to='mcs_taggroupid' from='mcs_taggroupid'>
+        <filter type='and'>
+            <condition attribute='mcs_code' value='9999' operator='ne'/>
+        </filter>
+    </link-entity>
   </entity>
 </fetch>"; 
                 return XDocument.Parse(fetchXml);
