83219f25956da7e7af63d50299b96b5b3c394a36
zuozhong <zz517115214@outlook.com>
2020-01-21 11:02:10 +0800

    预订订单

---
+++ DCEM.APP.UserCenter/src/app/page/carreserve/myreserveorder/list/list.page.ts
@@ -0,0 +1,28 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { DCore_Http, DCore_Page } from '../../../../../app/component/typescript/dcem.core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.page.html',
+  styleUrls: ['./list.page.scss'],
+})
+export class ListPage implements OnInit {
+    public tab: any = 'equityorder';
+    public model: any = {
+        search: {
+            apiUrl: "api/product/Detail",
+        },
+        title: "我的预订订单",
+        datadetail: {}
+    };
+    constructor(
+        private _http: DCore_Http,
+        private _page: DCore_Page,
+        private routerinfo: ActivatedRoute,
+    ) { }
+
+  ngOnInit() {
+  }
+
+}
