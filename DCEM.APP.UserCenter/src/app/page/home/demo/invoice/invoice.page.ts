c17489d288d93b1059b639a4469f62ea302a2625
zhangjunqiu <544048915@qq.com>
2020-01-21 14:40:30 +0800

    增值税发票识别demo

--- DCEM.APP.UserCenter/src/app/page/home/demo/invoice/invoice.page.ts
+++ DCEM.APP.UserCenter/src/app/page/home/demo/invoice/invoice.page.ts
@@ -26,6 +26,7 @@ export class InvoicePage implements OnInit {
       fpmc:"",
       fpzl:"",
       fpdm:"",
+      fphm:"",
       kprq:"",
       jym:"",
       gfmc:"",
@@ -62,6 +63,75 @@ export class InvoicePage implements OnInit {
   }
 
   ngOnInit() {
+    this.invoice(this.data.json);
   }
+  //增值税发票识别
+  invoice(json){
+    //this._page.loadingShow();
+    this._http.postCustom(
+        this.data.url,
+        json,
+        (res: any) => {
+            console.log(res);
+            if(res!=null && res.Code=="000"){
+              var item=res.Data;
+              if(item!=null){
+                this.data.mod.fpmc=item["发票名称"];
+                this.data.mod.fpzl=item["发票种类"];
+                this.data.mod.fpdm=item["发票代码"];
+                this.data.mod.fphm=item["发票号码"];
+                this.data.mod.kprq=item["开票日期"];
+                this.data.mod.jym=item["校验码"];
+                this.data.mod.gfmc=item["购方名称"];
+                this.data.mod.gfsbh=item["购方纳税人识别号"];
+                this.data.mod.gfdh=item["购方地址及电话"];
+                this.data.mod.gfzh=item["购方开户行及账号"];
+                this.data.mod.mmq=item["密码区"];
+                this.data.mod.xsfmc=item["销售方名称"];
+                this.data.mod.xsfsbh=item["销售方纳税人识别号"];
+                this.data.mod.xsfdh=item["销售方地址及电话"];
+                this.data.mod.xsfzh=item["销售方开户行及账号"];
+                this.data.mod.hjje=item["合计金额"];
+                this.data.mod.hjse=item["合计税额"];
+                this.data.mod.hjdx=item["价税合计大写"];
+                this.data.mod.hjxx=item["价税合计小写"];
+                this.data.mod.skr=item["收款人"];
+                this.data.mod.fh=item["复核"];
+                this.data.mod.kpr=item["开票人"];
+                this.data.mod.bz=item["备注"];
+                if(item["货物集合"]!=null && item["货物集合"].length>0){
+                  this.data.mod.list=[];
+                  for (let i = 0; i < item["货物集合"].length; i++) {
+                    var itm=item["货物集合"][i];
+                    this.data.mod.list.push({
+                      spmc:itm["货物名称"],
+                      ggxh:itm["规格型号"],
+                      dw:itm["单位"],
+                      sl:itm["数量"],
+                      dj:itm["单价"],
+                      je:itm["金额"],
+                      slv:itm["税率"],
+                      se:itm["税额"],
+                    });
+                    
+                  }
+                }
+              }
 
+
+            }
+            else {
+              this._page.alert("消息提示", res.Message);
+            }
+
+            //this._page.loadingHide();
+        },
+        (err: any) => {
+            console.log(err);
+            this._page.alert("消息提示", "发票信息识别错误");
+            //this._page.loadingHide();
+        }
+    );
+
+}
 }
