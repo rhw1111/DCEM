d07c1e11fb531863378b286c519c60f1b7c8f8e7
DESKTOP-UJ4AMUP\hexun <190563430@qq.com>
2020-02-20 14:01:06 +0800

    交车状态按钮控制

--- DCEM.MApp/dist/out-tsc/src/app/app.config.spec.js
+++
@@ -1,7 +0,0 @@
-import { AppConfig } from './app.config';
-describe('App.Config', () => {
-    it('should create an instance', () => {
-        expect(new AppConfig()).toBeTruthy();
-    });
-});
-//# sourceMappingURL=app.config.spec.js.map
