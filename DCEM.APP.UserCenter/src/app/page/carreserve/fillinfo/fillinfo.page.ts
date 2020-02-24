5e469b9341bccd19bfb2a37d380864c9ace79e93
zuozhong <zz517115214@outlook.com>
2020-02-24 10:59:35 +0800

    no message

--- DCEM.APP.UserCenter/src/app/page/carreserve/fillinfo/fillinfo.page.ts
+++ DCEM.APP.UserCenter/src/app/page/carreserve/fillinfo/fillinfo.page.ts
@@ -163,7 +163,7 @@ export class FillinfoPage implements OnInit {
         if ($("#fullname").hasClass("is-danger") || $("#mobile").hasClass("is-danger")) {
             return false;
         }
-        var gender = 0;
+        var gender = 1;
         $("div.label-radio").each(function (i, item) {
             if ($(item).hasClass("active")) {
                 gender = parseInt($(item).children("input").val().toString());
